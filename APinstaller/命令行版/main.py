5daa271a0aa926a5e0aace77e6063afa1b569ea9
liuding <mk11235g79@outlook.com>
2022-09-12 13:54:02 +0800

    version 1.01
    添加指令功能（目前只支持-d指令）

--- APinstaller/命令行版/main.py
+++ APinstaller/命令行版/main.py
@@ -3,6 +3,9 @@ import requests
 import tqdm
 import os
 
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+    }
 
 def Split(filesize, file_chunk=10):
     step = filesize//file_chunk
@@ -26,19 +29,36 @@ def download(Save_name, s_pos, e_pos):
                 Bar.update()
 
 
+argv = input("请输入参数（可选）:")
 url = input("请输入下载文件网址:")
-res = requests.head(url)
-save_name = os.path.basename(url)
-filesize = int(res.headers['Content-Length'])
-ranges = Split(filesize)
-print(f"分块数:{len(ranges)}")
-Bar = tqdm.tqdm(total=filesize, desc=f"下载文件{save_name}")
+if argv == "-d":
+    file_name = os.path.basename(url)
+    head = requests.head(url, headers=headers)
+    file_size = head.headers.get('Content-Length')
+    if file_size is not None:
+        file_size = int(file_size)
+    response = requests.get(url, headers=headers, stream=True)
+    chunk_size = 1024
+    bar = tqdm.tqdm(total=file_size, desc=f'下载文件 {file_name}')
+    with open(file_name, mode='wb') as f:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            f.write(chunk)
+            bar.update(chunk_size)
+    bar.close()
+    print(f"下载完成，文件名为：{file_name}")
+else:
+    res = requests.head(url)
+    save_name = os.path.basename(url)
+    filesize = int(res.headers['Content-Length'])
+    ranges = Split(filesize)
+    print(f"分块数:{len(ranges)}")
+    Bar = tqdm.tqdm(total=filesize, desc=f"下载文件{save_name}")
 
-with open(save_name, "wb") as f:
-    pass
-with ThreadPoolExecutor() as t:
-    futures = []
-    for s_pos, z_pos in ranges:
-        t.submit(download, save_name, s_pos, z_pos)
-    as_completed(futures)
-print(f"下载完成，文件名为{save_name}")
+    with open(save_name, "wb") as f:
+        pass
+    with ThreadPoolExecutor() as t:
+        futures = []
+        for s_pos, z_pos in ranges:
+            t.submit(download, save_name, s_pos, z_pos)
+        as_completed(futures)
+    print(f"下载完成，文件名为{save_name}")
